234f55d16a5775ac34e9585a5266c6876720b2d0
gruendike <45828607+gruendike@users.noreply.github.com>
2018-12-12 10:00:19 -0800

    Update PowerADGM.ps1

--- PowerADGM.ps1
+++ PowerADGM.ps1
@@ -8,14 +8,13 @@ function Get-GroupMembers {
 		foreach ($domain in $h.'domains'){
 			Get-QADGroupMember -Identity "$group" -Service "$domain" -OutVariable users | Out-Null
 			foreach ($user in $users){
-				if($user.Type -ne "foreignSecurityPrincipal"){
-					if($user.Type -eq "Group"){
-						$domain + "`t" + $h.level + $group + "`t" + $user.NTAccountName + "`t" + $user.displayname + "`t" + $user.Type + "`t" + $true
-						$pc = [regex]::Replace($user.ParentContainer, "^([^/]+).*",'$1')
-						Get-GroupMembers(@{"domains" = $pc; "groups" = $user.samaccountname; "nested" = $true; "level" = $h.level + $nested_symbol + " "})
-					} else {
-						$domain + "`t" + $h.level + $group + "`t" + $user.NTAccountName + "`t" + $user.displayname + "`t" + $user.Type + "`t" + $h.'nested'
-			}}}
+				if($user.Type -eq "Group"){
+					$domain + "`t" + $h.level + $group + "`t" + $user.NTAccountName + "`t" + $user.displayname + "`t" + $user.Type + "`t" + $true
+					$pc = [regex]::Replace($user.ParentContainer, "^([^/]+).*",'$1')
+					Get-GroupMembers(@{"domains" = $pc; "groups" = $user.samaccountname; "nested" = $true; "level" = $h.level + $nested_symbol + " "})
+				} else {
+					$domain + "`t" + $h.level + $group + "`t" + $user.NTAccountName + "`t" + $user.displayname + "`t" + $user.Type + "`t" + $h.'nested'
+			}}
 	}}
 	Remove-Variable * -ErrorAction SilentlyContinue
 }
